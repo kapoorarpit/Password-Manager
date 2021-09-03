d48ae761b33112603573b25b61721a4dd313e647
kapoorarpit <kapoorarpit2000@gmail.com>
2021-09-03 18:27:33 +0530

    _

--- main.js
+++ main.js
@@ -37,15 +37,9 @@ db.close((err) => {
 
 //Need a check-------------------------------------------------------------------------------------------------
 var db = new sqlite3.Database(__dirname + '/app.db');
-if(db==null){
-  db = new sqlite3.Database(':memory:', (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-  });
-}
 
+
+//section ends------------------------------------------------------------------------------------------------
 ipcMain.on("user-data",(event, arg)=>{
   console.log(arg);
 })
@@ -61,19 +55,56 @@ ipcMain.on('No-null-allowed', function(event){
 ipcMain.on('navigate-login', function(){
 mainWindow.loadFile('./nalika/login.html')
 })
+
+
 // login --------------------
 
 ipcMain.on('login', function(event,args){
+  //var db = new sqlite3.Database(__dirname + '/app.db');
   console.log("Login")
+  var query = "SELECT name FROM user WHERE name = '" + args[0]+"'";
+  var q=1
+  db.get(query,(err,rows)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    else {
+      if(rows==null){
+      dialog.showErrorBox('No Entry Found','Please register first')
+      q=2
+      return
+    }
+    }
+  })
+  if(q==2){
+  return}
+  var query = "SELECT password FROM user WHERE name = '" + args[0]+"'";
+  db.get(query,(err,rows)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    else if(rows!=null){
+      if(rows.password!=args[1]){
+      console.log(rows.name)
+      console.log(args[0])
+      dialog.showErrorBox('Password not matching','Please try again')}
+      else{
+        let modalPath= path.join(__dirname,'./nalika/index.html')
+        mainWindow.loadFile(modalPath)
+      }
+    }
+  })
   console.log(args)
 })
 
 
 
+
+
 // register --------------------
 ipcMain.on('register', function(event,args){
-
-
 //databse -----------------
 db.run('CREATE TABLE IF NOT EXISTS user(name TEXT PRIMARY KEY,password TEXT NOT NULL)',function(err){
   if(err){
@@ -85,14 +116,13 @@ db.run('CREATE TABLE IF NOT EXISTS user(name TEXT PRIMARY KEY,password TEXT NOT 
       return console.log(err.message); 
     }
   })
-  console.log("register")
 })
-db.run('CREATE TABLE IF NOT EXISTS entries(entry TEXT PRIMARY KEY,password TEXT NOT NULL, strength INT NOT NULL)',function(err){
+var query="CREATE TABLE IF NOT EXISTS "+args[0]+"_entries(entry TEXT PRIMARY KEY,password TEXT NOT NULL, strength INT NOT NULL)"
+db.run(query,function(err){
   if(err){
     console.log(err)
     return
   }
-  console.log("register")
 })
   console.log(args)
   //naviagate
@@ -112,15 +142,58 @@ db.run('CREATE TABLE IF NOT EXISTS entries(entry TEXT PRIMARY KEY,password TEXT 
   win.on('close', function () { win = null })
   win.loadFile(modalPath);
   win.show()
+  console.log("Line 107 'register' working")
 })
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 db.run(`DROP TABLE user;`)
 db.run(`DROP TABLE entries;`)
 */
+
+
+
+
+
+
 //checks all the tables
+
 db.serialize(function () {
   db.all("select name from sqlite_master where type='table'", function (err, tables) {
       console.log(tables);
   });
 })
+
+/*
+db.serialize(function() {
+
+  db.all("SELECT * FROM user", function(err, allRows) {
+
+      if(err != null){
+          console.log(err);
+      }
+      console.log(allRows)
+  });
+});
+
+db.serialize(function() {
+
+  db.all("SELECT * FROM entries", function(err, allRows) {
+
+      if(err != null){
+          console.log(err);
+      }
+      console.log(allRows)
+  });
+});
+*/
